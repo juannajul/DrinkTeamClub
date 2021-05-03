bb21283b3dd91c0a0a7950e91f1a3e59d799b933
juannajul <juannajulvillegas@gmail.com>
2021-05-02 21:40:45 -0400

    Inicie el diseño de index.html, mañana sigo en su desarrollo responsivo y de estilos

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("register/", views.register, name="signup"),
     path("login/", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
 ]
--- users/views.py
+++ users/views.py
@@ -53,3 +53,7 @@ def login_view(request):
             })
     else:
         return render(request, "../templates/users/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
