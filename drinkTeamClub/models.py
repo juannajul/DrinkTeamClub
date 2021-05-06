1fb6647d0a1d01d59ea96e50f7d0f6f0ac6ea046
juannajul <juannajulvillegas@gmail.com>
2021-05-06 12:29:28 -0400

    Cree drinkTeamClub model, diseño en el index: responsive, search cocktail forms

--- drinkTeamClub/models.py
+++ drinkTeamClub/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+# Import User model. 
+from users.models import User
+
 # Create your models here.
+
+class Ingredient(models.Model):
+    ingredient_name = models.CharField(max_length=60)
+    
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=60)
+
+class Drink(models.Model):
+    drink_name = models.CharField(max_length=60)
+    drink_instructions = models.CharField(max_length=60)
+    drink_image = models.URLField(blank=True, default="https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="cocktail_category")
+    favorites = models.ManyToManyField(User, related_name="drink_in_favorite", blank=True)
+    ingredients = models.ManyToManyField(Ingredient, related_name="drink_ingredients", blank=True)
--- drinkTeamClub/views.py
+++ drinkTeamClub/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
 def index(request):
+    return render(request, "../templates/drinkTeam/index.html")
+
+def random_cocktail(request):
+    url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+    r = requests.get(url)
+    r2 = r.json()
+    print("---------------------------")
+    #print(r.status_code)
+    print("---------------------------")
+    #print(r.headers)
+    print("---------------------------")
+    print(r)
+    print("---------------------------")
+    print(r.json())
+    print("---------------------------")
+    
+    drinks = r2['drinks']
+    print(drinks)
+    drink_list = []
+    drink_dict = {}
+    for drink in drinks:
+        print(drink)
+        dic = drink.copy()
+        for key, value in dic.items():
+            if value != None:
+                drink_dict[key] = value
+    print("---------------------------")
+    print(drink_dict)
     return render(request, "../templates/drinkTeam/index.html")
