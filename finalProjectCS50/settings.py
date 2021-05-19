e99ad49b1642a0dd29f44e0729f53056bb748742
juanNajul <juannajulvillegas@gmail.com>
2021-05-19 18:25:04 -0400

    last commit 1 version of drinkTeamClub

--- finalProjectCS50/settings.py
+++ finalProjectCS50/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'finalProjectCS50.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#   'default': {
+#      'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'drinkteam',
+        'USER': 'postgres',
+        'PASSWORD': '26584155',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
